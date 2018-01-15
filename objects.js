a1aa44ea82f50a7bc25fc67ccfc190e068ba7ef7
butch cosio <butchcosio@yahoo.com>
2018-01-15 20:20:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,6 @@ var playlist = {artistName: 'songList'};
 
 function updatePlaylist(playlist, artistName, songTitle){
   playlist.artistName = "Phil Ocs"
-  playlist.songList = 
-  
+  playlist.songList =  "Here's to the State of Mississippi"
   
 }
